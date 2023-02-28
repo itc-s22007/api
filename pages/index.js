1a89bad0e5abd20a513a50117b7050cc91b891be
s22007 <s22007>
2023-02-28 08:11:04 +0000

    neko&dog

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,44 @@
-const apiUrl = 'https://world.openfoodfacts.org/cgi/search.pl?search_terms='
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+export default function Home () {
+  const [dogImage, setDogImage] = useState('')
+  const [catImage, setCatImage] = useState('')
+  const apiUrl = 'https://api.thedogapi.com/v1/images/search'
+  const Apiurl = 'https://api.thecatapi.com/v1/images/search'
 
-async function searchProduct (productName) {
-  try {
-    const response = await fetch(apiUrl + encodeURIComponent(productName))
-    const data = await response.json()
-    const products = data.products
-    console.log(`Found ${products.length} products for "${productName}":`)
-    products.forEach((product) => {
-      console.log('- ' + product.product_name)
-      console.log('Ingredients:', product.ingredients_text)
-      console.log('Nutrition facts:', product.nutriments)
-    })
-  } catch (error) {
-    console.error(error)
+  useEffect(() => {
+    fetchDogImage()
+  }, [])
+
+  const fetchDogImage = async () => {
+    try {
+      const res = await axios.get(apiUrl)
+      setDogImage(res.data[0].url)
+    } catch (error) {
+      console.log(error)
+    }
   }
-}
+  useEffect(() => {
+    fetchCatImage()
+  }, [])
+  const fetchCatImage = async () => {
+    try {
+      const res = await axios.get(Apiurl)
+      setCatImage(res.data[0].url)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-searchProduct('chocolate')
+  return (
+    <div>
+      <h1>The dog only</h1>
+      <button onClick={fetchDogImage}>次へ</button>
+      {dogImage && <img src={dogImage} alt='dog' />}
+      <h1>The cat only</h1>
+      <button onClick={fetchCatImage}>次へ</button>
+      {catImage && <img src={catImage} alt='cat' />}
+    </div>
+
+  )
+}
